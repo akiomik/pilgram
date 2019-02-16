66c751582d304e1190d343a839eee67d4c72b728
Akiomi Kamakura <akiomik@gmail.com>
2019-02-16 10:52:26 +0900

    Improve rise filter

--- pilgram/rise.py
+++ pilgram/rise.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from image4layer import Image4Layer
-from PIL import Image, ImageEnhance, ImageChops
+from PIL import Image, ImageEnhance
 
 from pilgram import css
 from pilgram import util
@@ -31,32 +31,21 @@ def rise(im):
 
     cb = im.convert('RGB')
 
-    cs1 = util.fill(cb.size, [236, 205, 169])
-    cs2 = util.fill(cb.size, [50, 30, 7])
-    cs3 = util.fill(cb.size, [232, 197, 152])
+    cs1 = util.fill(cb.size, [236, 205, 169, .15])
+    cm1 = Image4Layer.multiply(cb, cs1)
+
+    cs2 = util.fill(cb.size, [50, 30, 7, .4])
+    cm2 = Image4Layer.multiply(cb, cs2)
 
     gradient_mask1 = util.radial_gradient_mask(cb.size, length=.55)
-    cs = Image.composite(cs1, cs2, gradient_mask1)
-    cs = ImageChops.multiply(cb, cs)
+    cm = Image.composite(cm1, cm2, gradient_mask1)
 
-    # TODO
-    alpha_mask1 = util.scale_color(gradient_mask1, .85)
-    cs = Image.composite(cb, cs, alpha_mask1)
+    cs3 = util.fill(cb.size, [232, 197, 152, .8])
+    cm3 = Image4Layer.overlay(cm, cs3)
 
-    alpha_mask2 = util.scale_color(gradient_mask1, .4)
-    cs = Image.composite(cb, cs, alpha_mask2)
-
-    # TODO
-    alpha_mask3 = util.radial_gradient_mask(cb.size, scale=.9)
-    gradient_mask2 = ImageChops.invert(alpha_mask3)
-    cs_ = Image.composite(cs, cs3, gradient_mask2)
-
-    cs_ = Image4Layer.overlay(cs, cs_)
-    cs_ = Image.composite(cs, cs_, alpha_mask3)
-
-    alpha_mask4 = util.scale_color(gradient_mask2, .2)
-    cs_ = Image.composite(cs, cs_, alpha_mask4)
-    cr = Image.blend(cs, cs_, .6)
+    gradient_mask2 = util.radial_gradient_mask(cb.size, scale=.9)
+    cm_ = Image.composite(cm3, cm, gradient_mask2)
+    cr = Image.blend(cm, cm_, .6)
 
     cr = ImageEnhance.Brightness(cr).enhance(1.05)
     cr = css.sepia(cr, .2)
